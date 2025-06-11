9108ad53df76780fbf08540fa72761159ffadba7
xisxus <shefain3@gmail.com>
2025-06-11 23:22:22 +0600

    EmployeeDetails Initially Added

---
+++ GCTL_App/Controllers/Employees/EmployeeDetailsController.cs
@@ -0,0 +1,21 @@
+﻿using GCTL.Service.Employees.EmployeeDetails;
+using GCTL.Service.Language;
+using GCTL.Service.UserProfile;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCTL_App.Controllers.Employees
+{
+    public class EmployeeDetailsController : BaseController
+    {
+        private readonly IEmployeeDetailsService _employeeDetailsService;
+        public EmployeeDetailsController(ITranslateService translateService, IUserProfileService userProfileService, IEmployeeDetailsService employeeDetailsService) : base(translateService, userProfileService)
+        {
+            _employeeDetailsService = employeeDetailsService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
