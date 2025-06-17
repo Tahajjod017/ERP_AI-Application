a6e7bf405e4872f7c739c81c932a0925bdf88406
Asaduzzaman <asadmax100@gmail.com>
2025-06-17 15:04:25 +0600

    addedAdminSystemSettings

---
+++ GCTL_App/Controllers/AdminSettings/CompanySettings/LocalizationSettingsController.cs
@@ -0,0 +1,18 @@
+﻿using GCTL.Service.Language;
+using GCTL.Service.UserProfile;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCTL_App.Controllers.AdminSettings.CompanySettings
+{
+    public class LocalizationSettingsController : BaseController
+    {
+        public LocalizationSettingsController(ITranslateService translateService, IUserProfileService userProfileService) : base(translateService, userProfileService)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
