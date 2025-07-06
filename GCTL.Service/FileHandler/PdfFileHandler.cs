c987f9829ff43ab5350a35e951ab59ac199a0c0b
xisxus <shefain3@gmail.com>
2025-07-06 13:08:53 +0600

    Pdf Added For Employee Profile

--- GCTL.Service/FileHandler/PdfFileHandler.cs
+++ GCTL.Service/FileHandler/PdfFileHandler.cs
@@ -5,9 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using GCTL.Core.Repository;
 using GCTL.Data.Models;
+using GCTL.Service.ImageFileHandler;
 using QuestPDF.Fluent;
 using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
+
 
 namespace GCTL.Service.FileHandler
 {
@@ -16,82 +18,15 @@ namespace GCTL.Service.FileHandler
 
         private readonly IGenericRepository<EmployeeOfficeInfo> _employeeOfficialRepository;
         private readonly IGenericRepository<Organization> _organizationRepository;
+        private readonly IImageFileHandlerService _imageHelper;
 
-        public PdfFileHandler(IGenericRepository<EmployeeOfficeInfo> employeeOfficialRepository, IGenericRepository<Organization> organizationRepository)
+        public PdfFileHandler(IGenericRepository<EmployeeOfficeInfo> employeeOfficialRepository, IGenericRepository<Organization> organizationRepository, IImageFileHandlerService imageHelper)
         {
             _employeeOfficialRepository = employeeOfficialRepository;
             _organizationRepository = organizationRepository;
+            _imageHelper = imageHelper;
         }
 
-        //public void ComposeHeader(IContainer container, int companyId , bool showOnce = false)
-        //{
-
-        //    // Define default values for fields not directly available
-        //    string companyName = "";
-        //    string companyAddress = "";
-        //    string companyImage = ""; // Default image path
-        //    var company = _organizationRepository.AllActive().Where(e => e.OrganizationID == companyId).FirstOrDefault();
-        //    if (company != null)
-        //    {
-        //        companyName = company.OrganizationName;
-        //        companyAddress = company.Address;
-        //        companyImage = company.LogoLink;
-        //    }
-
-        //    if (showOnce)
-        //    {
-        //        container.ShowOnce().PaddingBottom(10).Column(column =>
-        //        {
-        //            column.Item().Row(row =>
-        //            {
-        //                row.ConstantItem(50).Height(50).Element(logo =>
-        //                {
-        //                    logo.Padding(5).Image("wwwroot/uploads/company/logo/" + companyImage , ImageScaling.FitArea);
-        //                });
-
-        //                row.RelativeItem().AlignCenter().Column(centerCol =>
-        //                {
-        //                    centerCol.Item().AlignCenter().Text(companyName)
-        //                        .FontSize(20).SemiBold().FontColor(Colors.Blue.Medium);
-
-        //                    centerCol.Item().AlignCenter().Text(companyAddress)
-        //                        .FontSize(10).Light().FontColor(Colors.Grey.Darken2).LineHeight(1.3f);
-        //                });
-
-        //                row.ConstantItem(90);
-        //            });
-
-        //            column.Item().LineHorizontal(1).LineColor(Colors.Grey.Lighten1);
-        //        });
-        //    }
-        //    else
-        //    {
-        //        container.PaddingBottom(10).Column(column =>
-        //        {
-        //            column.Item().Row(row =>
-        //            {
-        //                row.ConstantItem(90).Height(90).Element(logo =>
-        //                {
-        //                    logo.Padding(5).Image("wwwroot/uploads/company/logo/" + companyImage, ImageScaling.FitArea);
-        //                });
-
-        //                row.RelativeItem().AlignCenter().Column(centerCol =>
-        //                {
-        //                    centerCol.Item().AlignCenter().Text(companyName)
-        //                        .FontSize(20).SemiBold().FontColor(Colors.Blue.Medium);
-
-        //                    centerCol.Item().AlignCenter().Text(companyAddress)
-        //                        .FontSize(10).Light().FontColor(Colors.Grey.Darken2).LineHeight(1.3f);
-        //                });
-
-        //                row.ConstantItem(90);
-        //            });
-
-        //            column.Item().LineHorizontal(1).LineColor(Colors.Grey.Lighten1);
-        //        });
-        //    }
-
-        //}
 
 
         public void ComposeHeader(IContainer container, int companyId, bool showOnce = false)
@@ -177,8 +112,31 @@ namespace GCTL.Service.FileHandler
         }
 
 
-       
+        public void ComposeWatermark(IContainer container, int companyId, float opacity = 0.1f)
+        {
+            var company = _organizationRepository.AllActive()
+                .FirstOrDefault(e => e.OrganizationID == companyId);
+            if (company == null || string.IsNullOrEmpty(company.OrganizationName))
+                return;
+            var companyName = company.OrganizationName.ToUpper();
 
+            container.Layers(layers =>
+            {
+                layers.PrimaryLayer()
+                 .AlignCenter()
+                    .AlignMiddle()
+                    .Rotate(-45)
+                    .Text(text =>
+                    {
+                        text.DefaultTextStyle(style => style
+                            .FontFamily("Arial")
+                            .FontSize(48)
+                            .FontColor(Color.FromHex("#000406").WithAlpha(1)));
 
+                        text.Span(companyName);
+                    })
+                   ; // Rotate -45 degrees for diagonal effect
+            });
+        }
     }
 }
