2c178664301f533d6fa9e54ad2e91f2f6638f31f
Md. Rakib Hasan <rhsrakib030@gmail.com>
2025-07-24 16:05:49 +0600

    Added JS

---
+++ GCTL.Core/ViewModels/AttendanceManagement/ScheduleManagement/OffDayRoster/RosterInOffDaySetupVM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCTL.Core.ViewModels.AttendanceManagement.ScheduleManagement.OffDayRoster
+{
+    public class RosterInOffDaySetupVM : BaseViewModel
+    {
+        public int RosterInOffDayID { get; set; }
+
+        [Required(ErrorMessage = "Organization is required!")]
+        public int? OrganizationID { get; set; }
+        public string? OrganizationName { get; set; }
+
+        public int? BranchID { get; set; }
+        public List<int>? BranchIDs { get; set; }
+        public string? BranchName { get; set; }
+
+        public int? DepartmentID { get; set; }
+        public List<int>? DepartmentIDs { get; set; }
+        public string? DepartmentName { get; set; }
+
+        public int? EmployeeID { get; set; }
+        public List<int>? EmployeeIDs { get; set; }
+        public string? EmployeeName { get; set; }
+
+        [Required(ErrorMessage = "Shift is required!")]
+        public int? ShiftID { get; set; }
+        public string? ShiftName { get; set; }
+
+        [Required(ErrorMessage = "Start date is required!")]
+        public DateTime? StartDate { get; set; }
+
+        [Required(ErrorMessage = "End date is required!")]
+        public DateTime? EndDate { get; set; }
+
+        public string? TimeRange { get; set; }
+    }
+}
---
+++ GCTL_App/wwwroot/js/pages/AttendanceManagement/ScheduleManagement/offDayRoster.js
@@ -0,0 +1,15 @@
+﻿(function ($) {
+    $.offdayroster = function (options) {
+        // Default options
+        var settings = $.extend({
+            baseUrl: '/',
+        }, options);
+
+        var gridUrl = settings.baseUrl + "/GetAll";
+        $(() => {
+
+
+        });
+
+    }
+}(jQuery));
