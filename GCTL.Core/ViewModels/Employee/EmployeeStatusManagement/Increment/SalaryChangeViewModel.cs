f89ddda90c13489637e5cc2a4375543ed44f07c2
xisxus <shefain3@gmail.com>
2025-07-29 17:25:46 +0600

    ff

---
+++ GCTL.Core/ViewModels/Employee/EmployeeStatusManagement/Increment/SalaryChangeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCTL.Core.ViewModels.Employee.EmployeeStatusManagement.Increment
+{
+    public class SalaryChangeViewModel
+    {
+        public int EmployeeId { get; set; }
+        public int OrganizationId { get; set; }
+        public int DesignationId { get; set; }
+        public int DepartmentId { get; set; }
+
+        public string ChangeType { get; set; } // "increment" or "decrement"
+        public DateTime EffectiveDate { get; set; }
+
+        public decimal CurrentSalary { get; set; }
+        public decimal NewSalary { get; set; }
+
+        public string Remarks { get; set; }
+    }
+
+}
--- GCTL_App/Controllers/Employees/EmployeeStatusManagement/IncrementManagement/IncrementListController.cs
+++ GCTL_App/Controllers/Employees/EmployeeStatusManagement/IncrementManagement/IncrementListController.cs
@@ -1,19 +1,74 @@
-﻿using GCTL.Service.Language;
+﻿using GCTL.Core.Repository;
+using GCTL.Data.Models;
+using GCTL.Service.Language;
 using GCTL.Service.UserProfile;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GCTL_App.Controllers.Employees.EmployeeStatusManagement.IncrementManagement
 {
     public class IncrementListController : BaseController
     {
-        public IncrementListController(ITranslateService translateService, IUserProfileService userProfileService) : base(translateService, userProfileService)
+        private readonly IGenericRepository<GCTL.Data.Models.Employees> _employeeRepository;
+        private readonly IGenericRepository<Organization> _organizationRepository;
+        private readonly IGenericRepository<Departments> _departmentRepository;
+        private readonly IGenericRepository<Designations> _designationRepository;
+        public IncrementListController(ITranslateService translateService, IUserProfileService userProfileService, IGenericRepository<GCTL.Data.Models.Employees> employeeRepository, IGenericRepository<Organization> organizationRepository, IGenericRepository<Departments> departmentRepository, IGenericRepository<Designations> designationRepository) : base(translateService, userProfileService)
         {
+            _employeeRepository = employeeRepository;
+            _organizationRepository = organizationRepository;
+            _departmentRepository = departmentRepository;
+            _designationRepository = designationRepository;
         }
 
         public IActionResult Index()
         {
             SetSmartPageCode(111800);
+
+            ViewBag.EmployeeDD = new SelectList(_employeeRepository.All().Select(e => new
+            {
+                id = e.EmployeeID,
+                name = e.FirstName + " " + e.LastName
+            }), "id", "name");
+
+            ViewBag.OrganizationDD = new SelectList(
+               _organizationRepository.All().Select(o => new { o.OrganizationID, o.OrganizationName }),
+               "OrganizationID",
+               "OrganizationName"
+           );
+
+
+
+            ViewBag.DepartmentDD = new SelectList(
+                _departmentRepository.All().Select(d => new { d.DepartmentID, d.DepartmentName }),
+                "DepartmentID",
+                "DepartmentName"
+            );
+
+            ViewBag.DesignationDD = new SelectList(
+                _designationRepository.All().Select(d => new { d.DesignationID, d.DesignationName }),
+                "DesignationID",
+                "DesignationName"
+            );
+
             return View();
         }
+
+        #region Get ALL For Table
+
+        [HttpPost]
+        public IActionResult GetIncrementList(string searchTerm, int? departmentId, string incrementType, string dateRange, int pageSize = 10, int pageNumber = 1)
+        {
+           // var result = _yourService.GetFilteredIncrementList(searchTerm, departmentId, incrementType, dateRange, pageSize, pageNumber);
+
+            return Json(new
+            {
+                success = true,
+               // data = result
+            });
+        }
+
+
+        #endregion
     }
 }
