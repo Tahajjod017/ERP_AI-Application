515fd415cca860fceeef4ad9d91fb3705f911a0d
Debanjan Paul <debanjandevelopment@gmail.com>
2025-08-04 18:38:09 +0600

    modify modal

---
+++ GCTL_App/wwwroot/js/pages/CRM/companyMap.js
@@ -0,0 +1,38 @@
+﻿let autocomplete;
+
+function initAutocomplete() {
+    console.log("Running");
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById("autocomplete"),
+        {
+            types: ["(cities)"], // or use ["geocode"] for full addresses
+            fields: ["address_components", "geometry"],
+        }
+    );
+
+    autocomplete.addListener("place_changed", () => {
+        const place = autocomplete.getPlace();
+
+        let city = "", state = "", country = "";
+
+        for (const component of place.address_components) {
+            const type = component.types[0];
+
+            if (type === "locality") {
+                city = component.long_name;
+            }
+            if (type === "administrative_area_level_1") {
+                state = component.short_name;
+            }
+            if (type === "country") {
+                country = component.long_name;
+            }
+        }
+
+        //document.getElementById("city").value = city;
+        //document.getElementById("state").value = state;
+        //document.getElementById("country").value = country;
+    });
+}
+
+window.initAutocomplete = initAutocomplete;
--- GCTL_App/wwwroot/js/pages/CRM/createLead.js
+++ GCTL_App/wwwroot/js/pages/CRM/createLead.js
@@ -1,126 +1,98 @@
 ﻿
 $(document).ready(function () {
 
-//#region Auto suggest
+    //#region Auto suggest
 
-let nationalities = [];
+    let nationalities = [];
 
 
 
-$.ajax({
-    url: '/CreateLead/GetNationalities',
-    method: 'GET',
-    success: function (data) {
-        console.log(data);
-        nationalities = data;
-    },
-    error: function () {
-        alert('Failed to load Contact Name');
+    $.ajax({
+        url: '/CreateLead/GetNationalities',
+        method: 'GET',
+        success: function (data) {
+            nationalities = data;
+        },
+        error: function () {
+            alert('Failed to load Contact Name');
+        }
+    });
+
+
+
+    function showSuggestions(query) {
+        const $list = $('#contactNameList');
+        const $noResults = $('#noResults');
+        $list.empty();
+        $noResults.hide();
+
+        if (!query) return;
+
+        const filtered = nationalities.filter(item =>
+            item.toLowerCase().includes(query.toLowerCase())
+        );
+
+        if (filtered.length > 0) {
+            filtered.forEach(item => {
+                $list.append(`<button type="button" class="list-group-item list-group-item-action contractName-item">${item}</button>`);
+            });
+        } else {
+            $noResults.show();
+        }
     }
-});
 
+    $('#ContactNameSearch').on('input', function () {
+        const query = $(this).val();
+        $('#searchResults').show();
+        $('#removeContactNameBtn').toggle(!!query);
+        showSuggestions(query);
+    });
 
+    $(document).on('click', '.contractName-item', function () {
+        const selected = $(this).text();
+        $('#ContactNameSearch').val(selected);
+        $('#searchResults').hide();
+        $('#removeContactNameBtn').show();
+    });
 
-function showSuggestions(query) {
-    const $list = $('#contactNameList');
-    const $noResults = $('#noResults');
-    $list.empty();
-    $noResults.hide();
+    $('#removeContactNameBtn').on('click', function () {
+        $('#ContactNameSearch').val('');
+        $('#contactNameList').empty();
+        $('#noResults').hide();
+        $('#searchResults').hide();
+        $(this).hide();
+    });
+    let targetTab = 'company';
 
-    if (!query) return;
-
-    const filtered = nationalities.filter(item =>
-        item.toLowerCase().includes(query.toLowerCase())
-    );
-
-    if (filtered.length > 0) {
-        filtered.forEach(item => {
-            $list.append(`<button type="button" class="list-group-item list-group-item-action contractName-item">${item}</button>`);
-        });
-    } else {
-        $noResults.show();
-    }
-}
-
-// On input typing
-$('#ContactNameSearch').on('input', function () {
-    const query = $(this).val();
-    $('#searchResults').show();
-    $('#removeContactNameBtn').toggle(!!query);
-    showSuggestions(query);
-});
-
-// On selecting a suggestion
-$(document).on('click', '.contractName-item', function () {
-    const selected = $(this).text();
-    $('#ContactNameSearch').val(selected);
-    $('#searchResults').hide();
-    $('#removeContactNameBtn').show();
-});
-
-// Clear selected value
-$('#removeContactNameBtn').on('click', function () {
-    $('#ContactNameSearch').val('');
-    $('#contactNameList').empty();
-    $('#noResults').hide();
-    $('#searchResults').hide();
-    $(this).hide();
-});
-    let targetTab = 'company'; // default
-
-    // Button to open Company tab
     $('#addNewContactNameBtn').on('click', function () {
         targetTab = 'company'; // set desired tab
         $('#addNationalityModal').modal('show');
-        console.log(targetTab)
         $('#increment-tab').removeClass('active');
         $('#increment').removeClass('active show');
         $('#promotion-tab').addClass('active');
         $('#promotion').addClass('active show');
     });
 
-    // Button to open Person tab
     $('#addNewContactNameBtn2').on('click', function () {
         targetTab = 'person';
         $('#addNationalityModal').modal('show');
-        console.log(targetTab);
         $('#promotion-tab').removeClass('active');
         $('#promotion').removeClass('active show');
         $('#increment-tab').addClass('active');
         $('#increment').addClass('active show');
     });
-// Show modal when clicking "Click Here"
-    // Track which tab to activate
-    //let targetTab = 'company'; // default
 
-    //// Button to open Company tab
-    //$('#addNewContactNameBtn').on('click', function () {
-    //    targetTab = 'company'; // set desired tab
-    //    $('#addNationalityModal').modal('show');
-    //});
+    $("#closeModal").on('click', function () {
+        $('#addNationalityModal').modal('hide');
+    });
+    $("#closeModal1").on('click', function () {
+        $('#addNationalityModal').modal('hide');
+    });
 
-    //// Button to open Person tab
-    //$('#addNewContactNameBtn2').on('click', function () {
-    //       = 'person';
-    //    $('#addNationalityModal').modal('show');
-    //});
 
-    //// When modal is fully shown, activate the correct tab
-    //$('#addNationalityModal').on('shown.bs.modal', function () {
-    //    if (targetTab === 'company') {
-    //        $('#increment-tab').removeClass('active');
-    //        $('#increment').removeClass('active show');
-
-    //        $('#promotion-tab').addClass('active');
-    //        $('#promotion').addClass('active show');
-    //    } else {
-    //        $('#promotion-tab').removeClass('active');
-    //        $('#promotion').removeClass('active show');
-
-    //        $('#increment-tab').addClass('active');
-    //        $('#increment').addClass('active show');
-    //    }
-    //});
+    // Call initMap when the script is loaded
+    //window.initMap = initMap;
+    //console.log("initMap is running2");
 
 
 
