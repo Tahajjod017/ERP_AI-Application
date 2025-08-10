251ec3d365eb81e1a8479c7dfdd81f4acd56c5f9
Md. Rakib Hasan <rhsrakib030@gmail.com>
2025-08-10 18:32:41 +0600

    Added Spiral pattern. Need to post data. Filtering is okay.

---
+++ GCTL.Core/ViewModels/AttendanceManagement/ScheduleManagement/AssignSpiralPattern/AssignSpiralPatternSetupVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCTL.Core.ViewModels.AttendanceManagement.ScheduleManagement.AssignSpiralPattern
+{
+    public class AssignSpiralPatternSetupVM : BaseViewModel
+    {
+        [Required(ErrorMessage = "{0} is required!"), DisplayName("Organization")]
+        public int OrganizationID { get; set; }
+
+        public List<int>? DepartmentIDs { get; set; }
+
+        public List<int>? EmployeeIDs { get; set; }
+
+        [Required(ErrorMessage = "{0} is required!"), DisplayName("Spiral Pattern Type")]
+        public int? SpiralPatternTypeID { get; set; }
+
+        [Required(ErrorMessage = "{0} is required!"), DisplayName("Spiral Pattern")]
+        public int SpiralPatternID { get; set; }
+
+        [Required(ErrorMessage = "{0} is required!"), DisplayName("Start Date")]
+        public DateTime? StartDate { get; set; }
+
+        [Required(ErrorMessage = "{0} is required!"), DisplayName("End Date")]
+        public DateTime? EndDate { get; set; }
+    }
+}
