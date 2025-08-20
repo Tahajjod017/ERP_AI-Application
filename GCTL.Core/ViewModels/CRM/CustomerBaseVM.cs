50d386cdca532095bed36320db83f12a0d2a79a6
Debanjan Paul <debanjandevelopment@gmail.com>
2025-08-20 18:29:30 +0600

    added LeadTable code and LeadDetails View and email unique check code

--- GCTL.Core/ViewModels/CRM/CustomerBaseVM.cs
+++ GCTL.Core/ViewModels/CRM/CustomerBaseVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,6 +39,8 @@ namespace GCTL.Core.ViewModels.CRM
 
         public string? OtherPhone { get; set; }
 
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+    ErrorMessage = "Invalid email format")]
         public string? Email { get; set; }
     }
 }
