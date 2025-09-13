009f6414b605df5723d0eb4b42223c5410b73e96
Asaduzzaman <asadmax100@gmail.com>
2025-09-13 18:23:44 +0600

    UpdatedPrBars

--- GCTL.Service/AttendanceManagement/EmployeeAttendence/IEmployeeAttendanceReport.cs
+++ GCTL.Service/AttendanceManagement/EmployeeAttendence/IEmployeeAttendanceReport.cs
@@ -16,6 +16,7 @@ namespace GCTL.Service.AttendanceManagement.EmployeeAttendence
         Task<PaginationService<Attendance, EmployeeAttendenceVM>.PaginationResult<EmployeeAttendenceVM>> GetAllAsync(int pageNumber = 1, int pageSize = 5, string searchTerm = "",
         string sortColumn = "HolidayID", string sortOrder = "desc", int? organizationID = null, int? employeeId = null, int? statusID = null, string? sortId = "");
         Task<EmployeeAttendenceVM> GetAttendanceDetailsAsync(int employeeId);
+        Task<EmployeeAttendenceVM> GetAttendanceProgressBarAsync(int employeeId);
         Task<List<PunchActivityDto>> GetEmployeePunchActivityAsync(int userId);
         Task<TimeOnly?> GetEmployeeFirstPunchInTimeAsync(int userId);
         Task<double> GetTotalHoursForWeek(int employeeId, int organizationId, int? organizationBranchId);
