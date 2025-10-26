27ef5429aeabf884ccd21e5ddc86fa6816389d99
Debanjan Paul <debanjandevelopment@gmail.com>
2025-10-26 18:34:39 +0600

    added multipage js function

--- GCTL_App/wwwroot/js/pages/CRM/Customer/customer.js
+++ GCTL_App/wwwroot/js/pages/CRM/Customer/customer.js
@@ -1,43 +1,34 @@
-﻿if (!window.customerJSLoaded) {
-    window.customerJSLoaded = true;
-    $(function () {
-        //#region priorityID
-        $('#CountryID').select2({
+﻿window.initCustomerForm = function (root) {
+    root = root || document;
+
+    const countrySelect = root.querySelector('#CountryID');
+    if (countrySelect && !countrySelect.dataset.select2Initialized) {
+        $(countrySelect).select2({
             placeholder: 'Select Country',
             ajax: {
                 url: '/CreateLead/GetPriorityList',
                 dataType: 'json',
                 delay: 250,
                 data: function (params) {
-                    return {
-                        search: params.term || '',
-                        page: params.page || 1
-                    };
+                    return { search: params.term || '', page: params.page || 1 };
                 },
                 processResults: function (data, params) {
                     params.page = params.page || 1;
-
                     return {
-                        results: data.items.map(item => ({
-                            id: item.value,
-                            text: item.label
-                        })),
-                        pagination: {
-                            more: data.hasMore
-                        }
+                        results: data.items.map(i => ({ id: i.value, text: i.label })),
+                        pagination: { more: data.hasMore }
                     };
                 },
                 cache: true
             },
+            width: '100%',
             language: {
                 noResults: function () {
-                    return $(
-                        `<span>Data not found. Create a <a id="createCustomer" href="#">Customer</a></span>`
-                    );
+                    return $('<span>Data not found. Create a <a id="createCustomer" href="#">Customer</a></span>');
                 }
-            },
-            width: '100%'
+            }
         });
-        //#endregion
-    });
-}
+        showDev("Hello")
+        countrySelect.dataset.select2Initialized = true;
+    }
+};
