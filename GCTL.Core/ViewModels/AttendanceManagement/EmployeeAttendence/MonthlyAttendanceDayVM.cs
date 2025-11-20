7483a03d219c4175dca8c24cfb3ed917683338ad
Asaduzzaman <asadmax100@gmail.com>
2025-11-20 18:19:21 +0600

    report

---
+++ GCTL.Core/ViewModels/AttendanceManagement/EmployeeAttendence/MonthlyAttendanceDayVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCTL.Core.ViewModels.AttendanceManagement.EmployeeAttendence
+{
+    public class MonthlyAttendanceDayVM
+    {
+        public string? In { get; set; }
+        public string? Out { get; set; }
+        public string? Break { get; set; }
+        public string? Late { get; set; }
+        public string? Early { get; set; }
+        public string? Ot { get; set; }
+        public string? Prod { get; set; }
+        public string? Status { get; set; }
+    }
+
+    public class MonthlyAttendanceCalendarVM
+    {
+        public string Month { get; set; } = default!; // "yyyy-MM"
+        public Dictionary<string, MonthlyAttendanceDayVM> Data { get; set; } = new();
+    }
+
+}
