b5a61dfb6222a3f08704151979aba708ba1c0a01
xisxus <shefain3@gmail.com>
2025-12-20 13:16:57 +0600

    Req Controoler added before Reverse

---
+++ GCTL.Core/ViewModels/POS/Requsition/AddRequisition/RequisitionListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCTL.Core.ViewModels.POS.Requsition.AddRequisition
+{
+    public class RequisitionListViewModel
+    {
+        public string SearchTerm { get; set; }
+        public int? ProjectFilter { get; set; }
+        public int? ProductTypeFilter { get; set; }
+        public string SortBy { get; set; }
+        public string SortOrder { get; set; } = "asc";
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public int TotalItems { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalItems / PageSize);
+
+        public List<RequisitionItemViewModel> Requisitions { get; set; } = new List<RequisitionItemViewModel>();
+    }
+}
--- GCTL.Service/POS/Requsition/INewRequisitionService.cs
+++ GCTL.Service/POS/Requsition/INewRequisitionService.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GCTL.Core.ViewModels;
+using GCTL.Core.ViewModels.POS.Requsition.AddRequisition;
 
 namespace GCTL.Service.POS.Requsition
 {
     public interface INewRequisitionService
     {
+        Task<PaginatedResultCommon<RequisitionItemViewModel>> GetRequisitionListAsync(int page, int pageSize, string search, string sortColumn, string sortDirection, int? projectId, int? productTypeId, int? empID, string? FromDat, string? ToDate);
+        Task<CommonReturnViewModel> SaveRequsitionAsync(CreateRequisitionViewModel model);
+
+
+        Task<CommonReturnViewModel> DeleteRequisitionAsync(int id, BaseViewModel? baseView, int? empID);
+        Task<EditRequisitionViewModel> GetRequisitionByIdAsync(int id, int? empID);
+        Task<CommonReturnViewModel> UpdateRequisitionAsync(EditRequisitionViewModel model, int? empID);
+        Task<byte[]> GeneratePDF(int orgid, int id, string? FromDate, string? ToDate);
+        Task<byte[]> GenerateXL(int orgid, int id, string? FromDate, string? ToDate);
     }
 }
