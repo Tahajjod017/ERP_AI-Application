e8bc4c96b9989bf55e6fb3f3508d78e214a3dde7
Tahajjod017 <thazzod.ahmed@gmail.com>
2026-01-08 19:00:42 +0600

    01-08-2025

--- GCTL.Service/FieldServices/Advanced Apporval/IAdvancedApprovalService.cs
+++ GCTL.Service/FieldServices/Advanced Apporval/IAdvancedApprovalService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GCTL.Core.ViewModels;
+using GCTL.Core.ViewModels.AttendanceManagement.LeaveManagements.LeaveRequest;
 using GCTL.Core.ViewModels.FieldServices;
 using GCTL.Data.Models;
 using GCTL.Service.Pagination;
@@ -15,5 +17,9 @@ namespace GCTL.Service.FieldServices.Advanced_Apporval
 
         Task<PaginationService<EmployeeAdvances, ApprovalGetALLVM>.PaginationResult<ApprovalGetALLVM>> GetAllAsync1(int pageNumber = 1, int pageSize = 5,
            string searchTerm = "", string sortColumn = "EmployeeAdvanceID", string sortOrder = "desc", int? mainAccId = null, string userId = "");
+
+        Task<CommonReturnViewModel> ApproveAsync(int employeeAdvanceID, string approvedBy);
+
+        Task<CommonReturnViewModel> DeclineAsync(int employeeAdvanceID, string declinedBy);
     }
 }
